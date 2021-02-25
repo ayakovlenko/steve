2f03fb663845daa3f5cb4edc2dcf14fa3e3b798a
tony <ayakovlenko@users.noreply.github.com>
2021-02-25 03:11:54 +0200

    Ignore null fields

--- src/main/java/de/rwth/idsg/steve/model/ChargePoint.java
+++ src/main/java/de/rwth/idsg/steve/model/ChargePoint.java
@@ -1,10 +1,12 @@
 package de.rwth.idsg.steve.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChargePoint {
 
     public final String id;
